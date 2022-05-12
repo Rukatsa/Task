1e338f3bbad779cdb2d1a4fda070976cbc6e4d29
Rukatsa <emmavassily@gmail.com>
2022-05-12 14:25:52 +0200

    repeatContent

--- hello.php
+++ hello.php
@@ -3,3 +3,11 @@ function hello(string $text ): void {
     echo $text;
 }
 hello("param3t3r");
+
+function repatContent(int $count=1): void{
+    for($i=0;$i<=$count;$i++){
+        hello("Hello Vassily");
+    }
+}
+repatContent();
+repatContent(5);
