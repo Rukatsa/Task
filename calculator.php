7dce80f9187adde36fde62a0cf3d5153befbb8dc
Rukatsa <emmavassily@gmail.com>
2022-05-13 17:23:14 +0200

    calculator 1-1

---
+++ calculator.php
@@ -0,0 +1,18 @@
+<?php
+function sum(float $num1,float $num2):float{
+    return $num1+$num2;
+}
+function dif(float $num1,float $num2):float{
+    return $num1-$num2;
+}
+function pro(float $num1,float $num2):float{
+    return $num1*$num2;
+}
+function quo(float $num1,float $num2):float{
+    return $num1/$num2;
+}
+echo "The sum =".sum(10,2)."<br>";
+echo "The different =".dif(10,2)."<br>";
+echo "The product =".pro(10,2)."<br>";
+echo "The quotient =".quo(10,2)."<br>";
+?>
