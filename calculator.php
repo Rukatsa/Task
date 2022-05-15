13f40dd0dd39d6b2c446f8ccc2c16c27c8072769
Rukatsa <emmavassily@gmail.com>
2022-05-15 15:59:02 +0200

    calculator 1-2

--- calculator.php
+++ calculator.php
@@ -15,4 +15,21 @@ echo "The sum =".sum(10,2)."<br>";
 echo "The different =".dif(10,2)."<br>";
 echo "The product =".pro(10,2)."<br>";
 echo "The quotient =".quo(10,2)."<br>";
-?>
+
+
+function calculator(float $num1,string $op,float $num2):float{
+    if (!($op=='+' OR $op=='-' OR $op=='*' OR $op=='/' )) {
+        return 0;
+    }
+    switch ($op) {
+        case '+':return sum($num1,$num2);
+        case '-':return dif($num1,$num2);
+        case '*':return pro($num1,$num2);
+        case '/':return quo($num1,$num2); 
+    }
+}
+echo "<br>";
+echo "The sum =".calculator(1,'+',2)."<br>";
+echo "The different =".calculator(1,'-',2)."<br>";
+echo "The product =".calculator(1,'*',2)."<br>";
+echo "The quotient =".calculator(1,'/',2)."<br>";
