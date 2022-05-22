54c387c0ad2586c75b8b5ebbe537957d288d29d6
Rukatsa <emmavassily@gmail.com>
2022-05-22 17:02:53 +0200

    tsk 9...

--- super.php
+++ super.php
@@ -13,8 +13,16 @@ function quo(float $num1,float $num2):float{
 }
 function calculator(string $formula):float{
     echo($formula);
+    preg_match_all('!\d+!',$formula,$m);
+    print_r($m);
+
+    preg_match_all('!\d+!',$formula,$op);
+    print_r($op);
+
+    return (0);
 }
-echo "The sum =".sum(10,2)."<br>";
-echo "The different =".dif(10,2)."<br>";
-echo "The product =".pro(10,2)."<br>";
-echo "The quotient =".quo(10,2)."<br>";
+echo calculator('2-5*10');
+// echo "The sum =".sum(10,2)."<br>";
+// echo "The different =".dif(10,2)."<br>";
+// echo "The product =".pro(10,2)."<br>";
+// echo "The quotient =".quo(10,2)."<br>";
